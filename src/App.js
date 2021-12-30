0b802036ebafa64cfb2266a9a50b4750da303090
ratoobi <69602231+ratoobi@users.noreply.github.com>
2021-12-29 23:48:28 -0300

    Fix navigation links

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import apiKey from "./config";
 import axios from "axios";
 
 // App components
-import Nav from "./components/Nav";
+import Search from "./components/Search";
 import PhotoContainer from "./components/PhotoContainer";
 
 class App extends Component {
@@ -13,7 +13,16 @@ class App extends Component {
     super();
     this.state = {
       photos: [],
+      catPhotos: [],
+      dogPhotos: [],
+      computerPhotos: [],
     };
+  }
+
+  componentDidMount() {
+    this.performSearch("cats");
+    this.performSearch("dogs");
+    this.performSearch("computers");
   }
 
   performSearch = (query) => {
@@ -22,6 +31,21 @@ class App extends Component {
         `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
       )
       .then((response) => {
+        if (query === "cats") {
+          this.setState({
+            catPhotos: response.data.photos.photo,
+          });
+        }
+        if (query === "dogs") {
+          this.setState({
+            dogPhotos: response.data.photos.photo,
+          });
+        }
+        if (query === "computers") {
+          this.setState({
+            computerPhotos: response.data.photos.photo,
+          });
+        }
         this.setState({
           photos: response.data.photos.photo,
         });
@@ -38,33 +62,45 @@ class App extends Component {
         <BrowserRouter>
           <Routes>
             <Route
-              exact
               path="/"
-              element={<Nav onSearch={this.performSearch} />}
+              element={
+                <>
+                  <Search onSearch={this.performSearch} />
+                  <PhotoContainer data={this.state.photos} />
+                </>
+              }
             />
             <Route
-              exact
               path="/cats"
-              element={<Nav onSearch={this.performSearch("cats")} />}
+              element={
+                <>
+                  <Search onSearch={this.performSearch} />
+                  <PhotoContainer data={this.state.catPhotos} />
+                </>
+              }
             />
+
             <Route
-              exact
               path="/dogs"
-              element={<Nav onSearch={this.performSearch("dogs")} />}
+              element={
+                <>
+                  <Search onSearch={this.performSearch} />
+                  <PhotoContainer data={this.state.dogPhotos} />
+                </>
+              }
             />
+
             <Route
-              exact
               path="/computers"
-              element={<Nav onSearch={this.performSearch("computers")} />}
-            />
-            <Route
-              exact
-              path="/:searchText"
-              element={<Nav onSearch={this.performSearch} />}
+              element={
+                <>
+                  <Search onSearch={this.performSearch} />
+                  <PhotoContainer data={this.state.computerPhotos} />
+                </>
+              }
             />
           </Routes>
         </BrowserRouter>
-        <PhotoContainer data={this.state.photos} />
       </div>
     );
   }
